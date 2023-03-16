15f24c4e72bb6ec99913a1524e795fc6400e1099
gupta-gururaj <76657561+gupta-gururaj@users.noreply.github.com>
2023-03-16 13:48:42 +0530

    JWT

---
+++ JWT/simple-api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	fmt.Println("Server - http://localhost:8081/")
+	handleRequests()
+}
